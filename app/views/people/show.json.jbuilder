c68f49d4085d7a291b76e32b7f687158db7afdec
sanjeev <sanjeev.impinge@gmail.com>
2014-12-22 16:21:46 +0530

    first commit

---
+++ app/views/people/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @person, :id, :name, :account_id, :created_at, :updated_at
---
+++ db/migrate/20141222090433_create_people.rb
@@ -0,0 +1,10 @@
+class CreatePeople < ActiveRecord::Migration
+  def change
+    create_table :people do |t|
+      t.string :name
+      t.integer :account_id
+
+      t.timestamps
+    end
+  end
+end
